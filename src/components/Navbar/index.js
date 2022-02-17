56f65ece80561fdfb050fe2a827c0c630263d16b
RogerioJunor <rogeriopcjr@gmail.com>
2022-02-17 02:05:43 -0300

    main and chats

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,28 +8,26 @@ function Navbar({ sidebarOpen, openSidebar }) {
             <div className="nav_icon" onClick={() => openSidebar()}>
                 <i className="fa fa-bars" aria-hidden="true"></i>
             </div>
-
             <div className="navbar__left">
-                <a href="#">Produtos</a>
-                <a href="#">Usuarios</a>
-                <a href="#" className="active_link">Admin</a>
+                <a href="#">Subscribers</a>
+                <a href="#">Video Management</a>
+                <a className="active_link" href="#">
+                    Admin
+                </a>
             </div>
-
             <div className="navbar__right">
                 <a href="#">
-                    <i className="fa fa-search"></i>
+                    <i className="fa fa-search" aria-hidden="true"></i>
                 </a>
                 <a href="#">
-                    <i className="fa fa-clock"></i>
+                    <i className="fa fa-clock-o" aria-hidden="true"></i>
                 </a>
-                <a href="#">
+                <a href="#!">
                     <img width="30" src={avatar} alt="avatar" />
                 </a>
             </div>
-
         </nav>
-    )
-
-}
+    );
+};
 
 export default Navbar;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,74 +4,79 @@ import logo from '../../assets/img/Bank_72.png'
 function Sidebar({sidebarOpen, closeSidebar}) {
 
     return (
-        <div className={sidebarOpen ? "sidebar-responsive" : ""} id="sidebar">
-            <div className="sidebar__title">
-                <div className="sidebar__img">
-                    <img src={logo} alt="logo" width="120" height="80" />
-                    <h1>InfinityBank</h1>
-                </div>
-                <i onClick={() => closeSidebar()}
-                className="fa fa-times"
-                id="sidebarIcon"
-                aria-hidden="true"
-                ></i>
+        <div className={sidebarOpen ? "sidebar_responsive" : ""} id="sidebar">
+          <div className="sidebar__title">
+            <div className="sidebar__img">
+              <img src={logo} alt="logo" />
+              <h1>Codersbite</h1>
             </div>
-            <div className="sidebar__menu">
-                <div className="sidebar__link active_menu_link">
-                    <i className="fa fa-minus-square"></i>
-                    <a href="#">Home</a>
-                </div>
-                <h2>ADMIN</h2>
-                <div className="sidebar__link">
-                    <i className="fa fa-tachometer"></i>
-                    <a href="#">Area administrativa</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-building"></i>
-                    <a href="#">Lojas</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-archive"></i>
-                    <a href="#">Produtos</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-bars"></i>
-                    <a href="#">Categorias</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-cutlery"></i>
-                    <a href="#">Pedidos</a>
-                </div>
-                <h2>PESSOAS</h2>
-                <div className="sidebar__link">
-                    <i className="fa fa-male"></i>
-                    <a href="#">Administradores</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-user-circle"></i>
-                    <a href="#">Usuarios</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-money"></i>
-                    <a href="#">Pagamentos e custos</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-tasks"></i>
-                    <a href="#">A plataforma</a>
-                </div>
-                <div className="sidebar__link">
-                    <i className="fa fa-file-text"></i>
-                    <a href="#">Politica de privacidade</a>
-                </div>
-                <div className="sidebar__logout">
-                    <i className="fa fa-power-off"></i>
-                    <a href="#">Log out</a>
-                </div>
+            <i
+              onClick={() => closeSidebar()}
+              className="fa fa-times"
+              id="sidebarIcon"
+              aria-hidden="true"
+            ></i>
+          </div>
+    
+          <div className="sidebar__menu">
+            <div className="sidebar__link active_menu_link">
+              <i className="fa fa-home"></i>
+              <a href="#">Dashboard</a>
             </div>
+            <h2>MNG</h2>
+            <div className="sidebar__link">
+              <i className="fa fa-user-secret" aria-hidden="true"></i>
+              <a href="#">Admin Management</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-building-o"></i>
+              <a href="#">Company Management</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-wrench"></i>
+              <a href="#">Employee Management</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-archive"></i>
+              <a href="#">Warehouse</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-handshake-o"></i>
+              <a href="#">Contracts</a>
+            </div>
+            <h2>LEAVE</h2>
+            <div className="sidebar__link">
+              <i className="fa fa-question"></i>
+              <a href="#">Requests</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-sign-out"></i>
+              <a href="#">Leave Policy</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-calendar-check-o"></i>
+              <a href="#">Special Days</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-files-o"></i>
+              <a href="#">Apply for leave</a>
+            </div>
+            <h2>PAYROLL</h2>
+            <div className="sidebar__link">
+              <i className="fa fa-money"></i>
+              <a href="#">Payroll</a>
+            </div>
+            <div className="sidebar__link">
+              <i className="fa fa-briefcase"></i>
+              <a href="#">Paygrade</a>
+            </div>
+            <div className="sidebar__logout">
+              <i className="fa fa-power-off"></i>
+              <a href="#">Log out</a>
+            </div>
+          </div>
         </div>
-    );
-
-
-}
+      );
+};
 
 export default Sidebar;
